f352856762e2eea342666f7224593a99608d669f
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-08-03 15:08:06 +0200

    Initial commit.

---
+++ src/main/java/com/enterprise/farmhands/MyFarmhandsAppApplication.java
@@ -0,0 +1,12 @@
+package com.enterprise.farmhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFarmhandsAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyFarmhandsAppApplication.class, args);
+  }
+}
