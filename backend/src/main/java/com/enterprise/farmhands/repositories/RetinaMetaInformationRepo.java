2cc3be1e0c9f584bd3828f6e1617a272c2a902c7
HelmutSteiner <helmut.steinersti2@gmail.com>
2018-08-10 16:46:55 +0200

    Removes JPA information for testing on Heroku.

--- backend/src/main/java/com/enterprise/farmhands/repositories/RetinaMetaInformationRepo.java
+++
@@ -1,7 +0,0 @@
-package com.enterprise.farmhands.repositories;
-
-import com.enterprise.farmhands.entities.RetinaMetaInformation;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RetinaMetaInformationRepo extends CrudRepository<RetinaMetaInformation, Long> {
-}
